ea14df5521572f48627beef71ba64c6ce784b0d7
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2025-10-23 12:05:39 +1300

    update the rest

--- analysis/beast/nestedsampling/print_marginal.py
+++ analysis/beast/nestedsampling/print_marginal.py
@@ -4,18 +4,24 @@ import re
 from collections import namedtuple
 from pathlib import Path
 
-Likelihood = namedtuple("Likelihood", ['Marginal', 'H_N', 'SD', 'Information'])
+Likelihood = namedtuple("Likelihood", ['Marginal', 'H_N', 'SD', 'Information', 'MaxESS'])
 
 
-is_regex = re.compile(r"""^Marginal likelihood: (.*) sqrt\(H/N\)=\((.*)\)=\?=SD=\((.*)\) Information: (.*)$""")
+is_regex1 = re.compile(r"""^Marginal likelihood: (.*) sqrt\(H/N\)=\((.*)\)=\?=SD=\((.*)\) Information: (.*)$""")
+is_regex2 = re.compile(r"""^Max ESS: (.*)$""")
 
 
 def read(filename):
+    lh, max_ess = None, None
     with open(filename, 'r') as handle:
         for line in handle:
-            if m:= is_regex.match(line):
-                yield Likelihood(*[float(f) for f in m.groups()])
-
+            if m:= is_regex1.match(line):
+                lh = [float(f) for f in m.groups()]
+            elif m:= is_regex2.match(line):
+                max_ess = [float(m.groups()[0])]
+    if max_ess:
+        lh.append(max_ess[0])
+    return Likelihood(*lh)
 
 
 if __name__ == '__main__':
@@ -23,8 +29,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("filenames", help='filename(s)', nargs="+")
     args = parser.parse_args()
-
-    print("%-30s\t%s\t%s\t%s\t%s" % ('Filename', 'Marginal Lh', 'H/N', 'SD', 'Information'))
+    
+    results = []
     for filename in args.filenames:
-        for r in read(filename):
-            print("%-30s\t%0.4f\t%0.4f\t%0.4f\t%0.4f" % (Path(filename).stem, r.Marginal, r.H_N, r.SD, r.Information))
+        r = read(filename)
+        out = "%-35s\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f" % (Path(filename).stem, r.Marginal, r.H_N, r.SD, r.Information, r.MaxESS)
+        results.append((r.Marginal, out))
+
+
+    print("%-35s\t%s\t%s\t%s\t%s\t%s" % ('Filename', 'Marginal Lh', 'H/N', 'SD', 'Information', 'MaxESS'))
+    for m, o in sorted(results, reverse=True):
+        print(o)
