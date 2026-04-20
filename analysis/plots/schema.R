f67afad0cf7da54a7fd9fbe489d77689bde879fd
Frederic Blum <frederic_blum@eva.mpg.de>
2026-04-20 10:56:31 +0200

    update schema

--- analysis/plots/schema.R
+++ analysis/plots/schema.R
@@ -34,34 +34,34 @@ ggplot(df) +
   ) +
   
   # Reverse x-axis (Years BP)
-  scale_x_reverse(name = "Years BP", limits = c(3100, -50)) +
+  scale_x_reverse(name = "Years BP", limits = c(3250, -50)) +
   
   # Colors (approximate to your figure)
   scale_fill_manual(values = c(
-    "Yarinacocha" = "#6666B3",
-    "Pacacocha"   = "#E06AB7",
-    "Cumancaya"   = "#D9775F",
+    "Yarinacocha" = "#777777",
+    "Pacacocha"   = "#d55f2b",
+    "Cumancaya"   = "#4E964F",
     "Caimito"     = "#5FA0D9",
     "Early origin"= "#7A9997",
-    "Recent origin"="#A6D95A"
+    "Recent origin"="#B6D95F"
   )) +
   
   # Clean theme
-  theme_classic(base_size = 16) +
+  theme_classic(base_size = 20) +
   theme(
     legend.position = "none",
     axis.title.y = element_blank()
   ) +
   
   # Horizontal divider
-  geom_hline(yintercept = 0.5, size = 1.5) +
+  geom_hline(yintercept = 0.5, linewidth = 1.5) +
   
   # Section labels
-  annotate("rect", xmin = 3100, xmax = 2300, ymin = 5, ymax = 6,
+  annotate("rect", xmin = 3250, xmax = 2150, ymin = 5, ymax = 6,
            fill = "white", color = "black", linewidth = 1) +
   annotate("text", x = 2700, y = 5.5, label = "Archaeological phases", size = 6) +
   
-  annotate("rect", xmin = 3100, xmax = 2300, ymin = -1, ymax = 0,
+  annotate("rect", xmin = 3250, xmax = 2150, ymin = -1, ymax = 0,
            fill = "white", color = "black", linewidth = 1) +
   annotate("text", x = 2700, y = -0.5, label = "Diversification of Pano", size = 6)
 
